828006640071e51c2259325da11457ff4cb6fcad
Глеб Сукристик <Gleb15a@gmail.com>
2023-03-29 16:51:17 +0300

    Added Rich Domain Model code representation

---
+++ Domain.Rich/Products/Product.cs
@@ -0,0 +1,40 @@
+﻿using Domain.Rich.Products.ValueObjects;
+
+namespace Domain.Rich.Products
+{
+    public sealed class Product
+    {
+        public Guid Id { get; private set; }
+
+        public string Name { get; private set; } = string.Empty;
+
+        public Money Money { get; private set; }
+
+        public Sku Sku { get; private set; }
+
+        private Product()
+        {     
+        }
+
+        public static Product? Create(string name, decimal money, string sku)
+        {
+            var moneyVal = Money.Create(money);
+            var skuVal = Sku.Create(sku);
+
+            if (moneyVal is null || skuVal is null)
+            {
+                return null;
+            }
+
+            var product = new Product
+            {
+                Id = Guid.NewGuid(),
+                Name = name,
+                Money = moneyVal,
+                Sku = skuVal
+            };
+
+            return product;
+        }
+    }
+}
---
+++ Domain.Rich/Products/ValueObjects/Money.cs
@@ -0,0 +1,21 @@
+﻿namespace Domain.Rich.Products.ValueObjects
+{
+    public record Money
+    {
+        private const decimal MAX_AMOUNT = 9999;
+
+        private Money(decimal value) => Value = value;
+
+        public decimal Value { get; init; }
+
+        public static Money? Create(decimal value)
+        {
+            if (value is <= 0 or > MAX_AMOUNT)
+            {
+                return null;
+            }
+
+            return new Money(value);
+        }
+    }
+}
